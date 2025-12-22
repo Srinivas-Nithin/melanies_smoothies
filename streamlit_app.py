cde2674f190bfa5f0ad017e1022e8b624cbb0d29
V. SRINIVAS NITHIN <57948747+Srinivas-Nithin@users.noreply.github.com>
2025-12-22 12:21:09 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ st.write('Here is your healthy food')
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('Search_ON))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('Search_ON'))
 st.dataframe(data=my_dataframe, use_container_width=True)
 st.stop()
                                                                                             
